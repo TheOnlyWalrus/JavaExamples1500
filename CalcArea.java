575643a7902d04ab34ab5693fb86bdc68fcb31ea
Alex <jonealex@kean.edu>
2019-09-06 15:08:34 -0400

    CalcArea Finished (Day 2)

--- CalcArea.java
+++ CalcArea.java
@@ -6,6 +6,14 @@ public class CalcArea
 	public static void main(String[] args)
 	{
 		Scanner input = new Scanner(System.in);
+		double radius;
+		double area;
+
+		System.out.print("Input Radius: ");
+		radius = input.nextDouble();
+		area = Math.PI * Math.pow(radius, 2);
+
+		System.out.println("The area of a circle with radius " + radius + " is " + area);
 	}
 
 }
