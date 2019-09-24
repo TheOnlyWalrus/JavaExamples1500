0646d66e13ccbe4f507d13da8dd0fc531abcd915
Alex <jonealex@kean.edu>
2019-09-24 14:34:17 -0400

    Week 4 Class 1 - Commit 1 - Chars

---
+++ Week4/class/ComparingChars.java
@@ -0,0 +1,37 @@
+public class ComparingChars
+{
+	public static void main(String[] args)
+	{
+		char a = 'a';
+		char b = 'b';
+		char A = 'A';
+		char B = 'B';
+
+		if (a == A)
+		{
+			System.out.println("a == A");
+		{
+		else
+		{
+			System.out.println("a != A");
+		}
+
+		if (a < b)
+		{
+			System.out.println("a < b");
+		}
+		else
+		{
+			System.out.println("a > b");
+		}
+
+		if (a < B)
+		{
+			System.out.println("a < B");
+		}
+		else
+		{
+			System.out.println("a > B");
+		}
+	}
+}
---
+++ Week4/class/SpecialCharTest.java
@@ -0,0 +1,7 @@
+public class SpecialCharTest
+{
+	public static void main(String[] args)
+	{
+		System.out.print("\t\thi there\tfoo \"this is a test\"\n");
+	}
+}
