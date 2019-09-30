8450a5042897a478b704b3280dec804fcbb55c71
Walrus <theonlywalrus01@gmail.com>
2019-09-30 17:20:33 -0400

    MISC - Euler start

---
+++ Misc/src/math/Euler.java
@@ -0,0 +1,13 @@
+package math;
+
+import java.util.Scanner;
+
+public class Euler
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		scanner.close();
+	}
+}
