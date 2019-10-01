aad2d0d6d4dccde06c549d9fb78d16b86374ffd8
Walrus <theonlywalrus01@gmail.com>
2019-10-01 15:04:48 -0400

    Changed a thingy

--- Week4/src/hw/Problem4_22.java
+++ Week4/src/hw/Problem4_22.java
@@ -16,19 +16,22 @@ public class Problem4_22
 		String s2 = scanner.next();
 		scanner.close();
 		
-		for (int i = 0; i < s1.length(); i++)
-		{
-			/* Searching for s2 in s1 */
-			if (i <= s1.length() - s2.length())
-			{
-				String find_pattern = s1.substring(i, i + s2.length());
-				if (find_pattern.equals(s2))
-				{
-					is_substring = true;
-					break;
-				}
-			}
-		}
+//		for (int i = 0; i < s1.length(); i++)
+//		{
+//			/* Searching for s2 in s1 */
+//			if (i <= s1.length() - s2.length())
+//			{
+//				String find_pattern = s1.substring(i, i + s2.length());
+//				if (find_pattern.equals(s2))
+//				{
+//					is_substring = true;
+//					break;
+//				}
+//			}
+//		}
+		
+		/* Instead of using a for loop, for this purpose you can use String.contains */
+		is_substring = s1.contains(s2);
 		
 		System.out.printf("Is %s a substring of %s? %b", s1, s2, is_substring);
 	}
