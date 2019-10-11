37c2d0b240f6fe6d0457ca2ebf51edaf5376a3d0
Walrus <theonlywalrus01@gmail.com>
2019-10-11 15:30:25 -0400

    Project Euler - Problem 9 Solved

---
+++ ProjectEuler/src/Problem9.java
@@ -0,0 +1,33 @@
+public class Problem9
+{
+	public static void main(String[] args)
+	{
+		int a;
+		int b;
+		int c;
+		int product;
+		
+		for (a = 1; a < 1000; a++)
+		{
+			for (b = 1; b < 1000; b++)
+			{
+				for (c = 1; c < 1000; c++)
+				{
+					int a2 = (int)Math.pow(a, 2);
+					int b2 = (int)Math.pow(b, 2);
+					int c2 = (int)Math.pow(c, 2);
+					
+					if (a2 + b2 == c2)
+					{
+						if (a + b + c == 1000)
+						{
+							product = a * b * c;
+							System.out.println(product);
+							System.exit(0);
+						}
+					}
+				}
+			}
+		}
+	}
+}
