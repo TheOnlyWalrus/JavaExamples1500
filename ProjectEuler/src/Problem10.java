70926cd756809d7f6c8e539c2f88a3a025f093b3
Walrus <theonlywalrus01@gmail.com>
2019-10-11 15:37:15 -0400

    Project Euler - Problem 10 Solved

---
+++ ProjectEuler/src/Problem10.java
@@ -0,0 +1,35 @@
+public class Problem10
+{
+	public static boolean isPrime(int n)
+	{
+		if (n < 2)
+			return false;
+		if (n == 2)
+			return true;
+		if (n % 2 == 0)
+			return false;
+		
+		for (int i = 3; i < (int)Math.sqrt(n) + 1; i++)
+		{
+			if (n % i == 0)
+				return false;
+		}
+		
+		return true;
+	}
+	
+	public static void main(String[] args)
+	{
+		long sum = 0;
+		
+		for (int n = 1; n < 2e6; n++)
+		{
+			if (isPrime(n))
+			{
+				sum += n;
+			}
+		}
+		
+		System.out.println(sum);
+	}
+}
