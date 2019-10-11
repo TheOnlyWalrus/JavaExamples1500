ad7e1ff7808f78812eb4ac06079240ee0d704114
Walrus <theonlywalrus01@gmail.com>
2019-10-11 15:08:59 -0400

    Project Euler - Problem 25 Solved

---
+++ ProjectEuler/src/Problem25.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+
+public class Problem25
+{
+	public static void main(String[] args)
+	{
+		BigInteger f1 = new BigInteger("1");
+		BigInteger f2 = new BigInteger("1");
+		BigInteger fib = f1;
+		int index = 2;
+		
+		while (("" + fib).length() < 1000)
+		{	
+			fib = f1.add(f2);
+			f1 = f2;
+			f2 = fib;
+			
+			index += 1;
+		}
+		
+		System.out.println(index);
+	}
+}
