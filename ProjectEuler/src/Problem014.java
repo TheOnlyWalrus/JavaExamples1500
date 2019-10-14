9991bb12206da9892711ea9b57a399c876e6349e
Walrus <theonlywalrus01@gmail.com>
2019-10-14 09:26:51 -0400

    Update Problem014.java

--- ProjectEuler/src/Problem014.java
+++ ProjectEuler/src/Problem014.java
@@ -2,6 +2,9 @@ public class Problem014
 {	
 	public static void main(String[] args)
 	{
-		/* Done using python -- Not on github */
+		/*
+		 *  Done using python -- Not on github
+		 *  Python proved to be much faster for this problem
+		 */
 	}
 }
