b98850cbc1821c15c4b28c3cb28db27b54c46152
Walrus <theonlywalrus01@gmail.com>
2019-10-15 12:56:50 -0400

    Project Euler - Problems 24, 30 Solved

---
+++ ProjectEuler/src/Problem024.java
@@ -0,0 +1,27 @@
+public class Problem024
+{	
+	static int count = 0;
+	
+	private static void perms(String s, String remaining)
+	{
+		if (remaining.length() == 0)
+		{
+			count++;
+			if (count == (int)1e6)
+				System.out.println(s);
+		}
+		for (int i = 0; i < remaining.length(); i++)
+		{
+			String n = s + remaining.charAt(i);
+			
+			String nR = remaining.substring(0, i) + remaining.substring(i + 1);
+			
+			perms(n, nR);
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		perms("", "0123456789");
+	}
+}
---
+++ ProjectEuler/src/Problem030.java
@@ -0,0 +1,27 @@
+public class Problem030
+{
+	public static void main(String[] args)
+	{
+		int sum = 0;
+		
+		for (int i = 2; i < (int)1e6; i++)
+		{
+			String num = Integer.toString(i);
+			int sumOf5thPowers = 0;
+			
+			for (int j = 0; j < num.length(); j++)
+			{
+				int number = Integer.parseInt("" + num.charAt(j));
+				
+				sumOf5thPowers += Math.pow(number, 5);
+			}
+			
+			if (num.equals("" + sumOf5thPowers))
+			{
+				sum += i;
+			}
+		}
+		
+		System.out.println(sum);
+	}
+}
