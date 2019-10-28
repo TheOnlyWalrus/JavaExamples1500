1c248794a245c5e7cd94d015b8573e124a64564a
Walrus <theonlywalrus01@gmail.com>
2019-10-28 14:05:15 -0400

    Misc - ComplexNumber

--- Misc/src/TestPackages.java
+++ Misc/src/TestPackages.java
@@ -1,9 +1,12 @@
 import chemistry.CommonCompounds;
+import math.ComplexNumber;
 
 public class TestPackages
 {
 	public static void main(String[] args)
 	{	
 		System.out.println(new CommonCompounds.Water(1).getCharge());
+		
+		System.out.println(new ComplexNumber(2, 4).compareTo(new ComplexNumber(2, 2)) == 0);
 	}
 }
