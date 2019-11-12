3b6b23d2948f10fa5f84f88c6c13a5031b78602e
Walrus <theonlywalrus01@gmail.com>
2019-11-12 13:16:17 -0500

    Misc - Removed chemistry package - Created chemistry equation/constants

--- Misc/src/chemistry/Base.java
+++
@@ -1,6 +0,0 @@
-package chemistry;
-
-public class Base
-{
-	
-}
---
+++ Misc/src/math/Constants.java
@@ -0,0 +1,16 @@
+package math;
+
+public interface Constants
+{
+	public final double C = 3e8;
+	public final double speedOfLight = 3e8;
+	public final double NA = 6.022e23;
+	public final double avogadro = 6.022e23;
+	public final double h = 6.626e-34;
+	public final double planck = 6.626e-34;
+	public final double a0 = 5.29e-11;
+	public final double bohrRadius = 5.29e-11;
+	public final double RH = 2.178e-18;
+	public final double rydberg = 2.178e-18;
+	public final double electronMass = 9.1e-31;
+}
