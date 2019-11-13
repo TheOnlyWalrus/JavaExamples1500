8bf2b3f180e38a557e27efa6282194444a9ce4fd
Walrus <theonlywalrus01@gmail.com>
2019-11-13 09:41:49 -0500

    Week 11 Lecture 1 - JFrame Snow

---
+++ Week11/src/lecture/FrameTesting.java
@@ -0,0 +1,99 @@
+package lecture;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class FrameTesting extends JFrame
+{
+	int[] x;
+	int[] y;
+	int[] size;
+	Color[] colors;
+	int n = 0;
+	int nmax = 1000;
+	
+	public FrameTesting()
+	{
+		initUI();
+		
+		x = new int[nmax];
+		y = new int[nmax];
+		size = new int[nmax];
+		colors = new Color[nmax];
+	}
+	
+	public void sleep(int ms)
+	{
+		try
+		{
+			Thread.sleep(ms);
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private void initUI()
+	{
+		setTitle("Example");
+		setSize(300, 300);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public void paint(Graphics g)
+	{
+		BufferedImage buffer = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+		Graphics2D g2 = buffer.createGraphics();
+		
+		g2.setColor(new Color(0, 200, 255));
+		g2.fillRect(0, 0, getWidth(), getHeight());
+		
+		for (int i = 0; i < n; i++)
+		{
+			Color snowColor = colors[i];
+			g2.setColor(snowColor);
+			g2.drawLine(x[i] - size[i] / 2, y[i] - size[i] / 2, x[i] + size[i] / 2, y[i] + size[i] / 2);
+			g2.drawLine(x[i] + size[i] / 2, y[i] - size[i] / 2, x[i] + size[i] / 2, y[i] - size[i] / 2);
+		}
+		
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.drawImage(buffer, null, 0, 0);
+	}
+	
+	public static void main(String[] args)
+	{
+		FrameTesting ex = new FrameTesting();
+        ex.setVisible(true);
+
+        for(int i = 0; i < ex.nmax; i++)
+        {
+        	if (ex.n < ex.nmax)
+        	{
+        		ex.y[ex.n] = 0;
+        		ex.x[ex.n] = (int)(ex.getWidth() * Math.random());
+        		ex.size[ex.n] = (int)((Math.random() + 1) * 5);
+        		int scale = (int)(200 + 55 * Math.random());
+        		ex.colors[ex.n] = new Color(scale, scale, scale);
+        		ex.n++;
+        	}
+        	
+        	for (int j = 0; j < ex.n; j++)
+        	{
+        		if (ex.y[j] < ex.getHeight() - 15)
+        		{
+        			ex.y[j] += ex.size[i] / 4;
+        		}
+        	}
+        	
+        	ex.sleep(10);
+        	ex.repaint();
+        }
+	}
+}
