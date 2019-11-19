58d57b9e760b359b2843f323798f94690e05a06d
Walrus <theonlywalrus01@gmail.com>
2019-11-19 13:23:22 -0500

    Pixelation fix

--- Misc/src/img_manip/Test.java
+++ Misc/src/img_manip/Test.java
@@ -6,7 +6,7 @@ public class Test
 {
 	public static void main(String[] args)
 	{
-		Pixelate img = new Pixelate(new File("/Users/alexanderjones/Dropbox/Programming1500/Misc/src/img_manip/cat.png"), 5);
-		img.pixelate("/Users/alexanderjones/Dropbox/Programming1500/Misc/src/img_manip/pixel_cat", "png");
+		Pixelate img = new Pixelate(new File("src/img_manip/cat.png"), 5);
+		img.pixelate("src/img_manip/pixel_cat", "png");
 	}
 }
