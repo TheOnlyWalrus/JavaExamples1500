be8ac5ba590c8cb5a75b4a0a741f02becbabe655
Neb <jonealex@kean.edu>
2020-05-31 20:08:18 -0400

    Removed irrelevent files

--- LewisDot/src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test
-{
-	public static void main(String[] args)
-	{
-		Lewis2D lewis = new Lewis2D(new Element(1, 1, "H", "Hydrogen"), new Element(1, 1, "H", "Hydrogen"));
-		lewis.generate();
-	}
-}
